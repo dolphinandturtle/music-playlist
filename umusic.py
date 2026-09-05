d98436657cd0859e8d632f700c0fbd13039d562b
Dolphin & Turtle <iamreallybadatphysics@gmail.com>
2026-09-05 18:33:29 +0200

    completed 'replica' by 'oneohtrix point never'

--- umusic.py
+++ umusic.py
@@ -24,26 +24,23 @@ for song in music:
     filetitle = '.'.join(rest)
     metadata = []
     if "title" in song:
-        #metadata.append("-metadata " + "title" + '=' + '"' + song["title"].replace("\"", "\\\"").replace("$", "\\$") + '"')
         title = song["title"].replace("\"", "\\\"").replace("$", "\\$")
         metadata.append(f"--title \"{title}\"")
     if "author" in song:
         if isinstance(song["author"], str):
             author = song["author"]
         elif isinstance(song["author"], list):
-            author = ', '.join(song["author"])
+            OPUS_SEPARATOR = '-'
+            author = OPUS_SEPARATOR.join(song["author"])
         else:
             raise Exception
         author = author.replace("\"", "\\\"").replace("$", "\\$")
         metadata.append(f"--artist \"{author}\"")
-        #metadata.append("-metadata " + "artist" + '=' + '"' + author.replace("\"", "\\\"").replace("$", "\\$") + '"')
     if "album" in song:
         album = song["album"].replace("\"", "\\\"").replace("$", "\\$")
         metadata.append(f"--album \"{album}\"")
-        #metadata.append("-metadata " + "album" + '=' + '"' + song["album"].replace("\"", "\\\"").replace("$", "\\$") + '"')
     if "date" in song:
         metadata.append(f"--date \"{song['date']}\"")
-        #metadata.append("-metadata " + "date" + '=' + '"' + song["date"].replace("\"", "\\\"").replace("$", "\\$") + '"')
     if "queue" in song:
         metadata.append(f"--tracknumber \"{song['queue']}\"")
     if "cover" in song:
@@ -52,9 +49,7 @@ for song in music:
         _title = '.'.join(_rest)
         image_jpg = f"uimage/{_title.split('/')[-1]}.jpg"
         metadata.append(f"--picture \"{image_jpg}\"")
-        #metadata.append("-metadata " + "track" + '=' + '"' + str(song["queue"]).replace("\"", "\\\"").replace("$", "\\$") + '"')
     metadata = ' '.join(metadata)
-    #calls.append(f"ffmpeg -i {song['source']} -map_metadata -1 -c copy {metadata} -c:a libopus -b:a 64k umusic/{filetitle.split('/')[-1]}.opus")
     calls.append(f"(ffmpeg -i {song['source']} -map_metadata -1 -map a umusic/{filetitle.split('/')[-1]}.flac && opusenc --quiet --bitrate 64k {metadata} umusic/{filetitle.split('/')[-1]}.flac umusic/{filetitle.split('/')[-1]}.opus && rm umusic/{filetitle.split('/')[-1]}.flac)")
 
 calls_images = []
